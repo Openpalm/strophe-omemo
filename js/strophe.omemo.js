caa7784842516a0dffec8eb780b944a225421fe8
magog <magog@localhost.localdomain>
2017-09-24 22:38:32 +0200

    rollup set up

--- js/strophe.omemo.js
+++ js/strophe.omemo.js
@@ -1,5 +1,16 @@
 import { $iq, Strophe } from 'strophejs/strophe.js';
 import * as libsignal from 'libsignaljs/libsignal.js';
+import debug from 'debug';
+const log = debug('app:log');
+
+if (ENV !== 'production') {
+
+  // Enable the logger.
+  debug.enable('*');
+  log('Logging is enabled!');
+} else {
+  debug.disable();
+}
 
 Strophe.addNamespace('OMEMO', 'eu.siacs.conversations.axolotl');
 
