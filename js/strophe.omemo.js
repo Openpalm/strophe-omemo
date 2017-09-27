7251de1cbb4cd2d01452ab64e894d84a35230030
gog <gog@localhost.localdomain>
2017-09-27 19:19:24 +0200

    loaded libsig. asm.js issues w/ firefox resolved

--- js/strophe.omemo.js
+++ js/strophe.omemo.js
@@ -1,28 +1,41 @@
 //import { $iq, Strophe } from './strophe.js';
-import * as libsignal from './libsignal-protocol.js';
-import SignalProtocolStore from './libs/libsignaljs/test/InMemorySignalProtocolStore.js'
+import SignalProtocolStore from './libs/libsignaljs/test/InMemorySignalProtocolStore.js' //probably init in browser.html and pass it on.
 import $ from 'jquery'
+import 'crypto' from 'crypto'
 
-//import debug from 'debug';
-//const log = debug('app:log');
-
-//debug.enable('*');
-//log('Logging is enabled!');
-//
 Strophe.addNamespace('OMEMO', 'eu.siacs.conversations.axolotl');
 
 var omemo = {
   _connection: null,
   _storage: window.localStorage
+  _bundle: null // safe here?
+  _cipher: null // pass in gcm function. (window.crypto or window.msCrypto)
 };
+
 
 omemo.init = function(conn) {
   this._connection = conn;
-
   //@TODO maybe setup
-
   conn.addHandler(this._onMessage.bind(this), null, 'message');
 }
+omemo.restore = function(file) {
+  //  takes in an encrypted file that contains session information
+  //  restores a session for a user
+}
+omemo.serialize = function(file) {
+  // serialize the current session into a restorable format 
+}
+omemo.IV = function(IV) {
+  //set the IV
+}
+omemo.messageKey = function(messageKey) {
+  //set the messageKey
+}
+omemo.setUpMessageElements = function(text) {
+  //set the message elements
+}
+
+
 
 omemo.createEncryptedStanza = function(to, plaintext) {
   var encryptedStanza = new Strophe.Builder('encrypted', {
@@ -35,7 +48,9 @@ omemo.createEncryptedStanza = function(to, plaintext) {
 }
 
 omemo._onMessage = function(stanza) {
-  //@TODO
+  //@TODO 
+  //@preKeySignalMessage
+  //@
 
   $(document).trigger('msgreceived.omemo', [decryptedMessage, stanza]);
 }
