1df420e868cb166444ba71e86a88c944cfed2d55
shokodemon <boo.factor@gmail.com>
2018-01-25 19:21:23 +0100

    on_device done, fetch_bundle done

--- omemo/strophe.omemo.js
+++ omemo/strophe.omemo.js
@@ -1,14 +1,20 @@
 let codec = require('./codec.js')
 let symCipher = require('./gcm.js')
-let $ = require('jquery')
-
+//let $ = require('jquery')
 
 //let symCipher = require('./EAX.js')
 //let symCipher = require('./xChaCha20.js')
 
-Strophe.addConnectionPlugin('omemo', {
+let record = {
+    jid: null,
+    devices: null
+}
+
+let users = {}
+
+let omemo = {
     init: function (conn) {
-        this.connection = conn;
+        omemo.connection = conn;
 
         Strophe.addNamespace('OMEMO',         
             'eu.siacs.conversations.axolotl')
@@ -17,77 +23,73 @@ Strophe.addConnectionPlugin('omemo', {
         Strophe.addNamespace('OMEMO_DEVICELIST', 
             'eu.siacs.conversations.axolotl.devicelist')
 
-        this.publish = this.connection.pep.publish
-        this.subscribe = this.connection.pep.subscribe
+        omemo.publish = omemo.connection.pep.publish
+        //        subscribe @ app.connected in the main application logic file xmpplore.js
+        //        this.subscribe(Strophe.NS.OMEMO_DEVICELIST)                   
 
     },
     setup: function (jid) {
-        let ctxt = this
         if (!localStorage.getItem(jid)) {
-            var id = this.new_id()
+            var id = omemo.gen_id()
             console.log(id)
             localStorage.setItem(jid, id)
         }
-        this._jid = jid
-        this._id = id
+        omemo._jid = jid
+        omemo._id = id
         try { 
             if (libsignal) {
-                console.log("loaded") 
             } 
         } catch(e) { 
             throw new Error ("Libsignal-Protocol not found")
         }
         try { 
             if (SignalProtocolStore) {
-                this.connection._signal_store = new SignalProtocolStore(jid, id)
+                omemo.connection._signal_store = new SignalProtocolStore(jid, id)
             } 
         } catch(e) { 
             throw new Error ("SignalProtocolStore not found")
         }
-        try { 
-            if (OmemoStore) {
-                this.connection._omemo_store = new SignalProtocolStore(jid, id)
-            } 
-        } catch(e) { 
-            throw new Error ("SignalProtocolStore not found")
-        }
+        //   try { 
+        //       if (OmemoStore) {
+        //           omemo.connection._omemo_store = new OmemoStore(jid, id) //probably not needed
+        //       } 
+        //   } catch(e) { 
+        //       throw new Error ("SignalProtocolStore not found")
+        //   }
 
-        return Promise.all([this.arm()]).then(e => {
+        Promise.all([omemo.arm()]).then(e => {
             console.log("armed") 
-            //bundle && device headline handler
-            ctxt.connection.addHandler(
-                ctxt.on_headline,
+            omemo.connection.addHandler(
+                omemo.on_headline,
                 null,
                 "message",
                 "headline")
-            ctxt.connection.addHandler(
-                ctxt.on_bundle,
+            omemo.connection.addHandler(
+                omemo.on_bundle,
                 null,
                 "iq",
                 "result",
                 "fetch1"
             )
-
-            ctxt.connection.pep.subscribe(Strophe.NS.OMEMO_DEVICELIST)
-            ctxt.publish_device()
         })
     },
+    on_success: function () { 
+        omemo.publish_bundle()
+        omemo.publish_device()
+        return false
+    },
     arm: function () {
-        console.log("omemo first use")
-        var ctxt = this
         let kh  = libsignal.KeyHelper
         let registrationId = ''
         Promise.all([
             kh.generateIdentityKeyPair(),
-            ctxt.genPreKeys(100)
+            omemo.gen_prekeys(100)
         ]).then(function(result) {
             let identity = result[0];
-            registrationId = ctxt._id
-            console.debug(ctxt.connection)
-            ctxt.connection._signal_store.put('registrationId', registrationId)
-            ctxt.connection._signal_store.put('identityKey', result[0])
-            //double work refactor later
-            ctxt.connection._signal_store.getIdentityKeyPair().then((ikey) =>
+            registrationId = omemo._id
+            omemo.connection._signal_store.put('registrationId', registrationId)
+            omemo.connection._signal_store.put('identityKey', result[0])
+            omemo.connection._signal_store.getIdentityKeyPair().then((ikey) =>
                 kh.generateSignedPreKey(ikey, 1)).then((skey) => {
                     let key = {
                         pubKey: skey.keyPair.pubKey,
@@ -95,56 +97,45 @@ Strophe.addConnectionPlugin('omemo', {
                         keyId: skey.keyId,
                         signature: skey.signature
                     }
-                    ctxt.connection._signal_store.storeSignedPreKey(1, key)
-                    ctxt.publish_bundle()
+                    omemo.connection._signal_store.storeSignedPreKey(1, key)
                 })
-            ctxt._address = new libsignal.SignalProtocolAddress(ctxt._jid, ctxt.connection._signal_store.get('registrationId'));
+            omemo._address = new libsignal.SignalProtocolAddress(omemo._jid, omemo.connection._signal_store.get('registrationId'));
         }).then( o => { 
-            ctxt.connection._signal_store.setLocalStore(ctxt._jid, ctxt._id)
-            console.log("omemo is ready.")
+            omemo.connection._signal_store.setLocalStore(omemo._jid, omemo._id)
+
         })
+
+
     },
     on_bundle: function (stanza) {
-        console.log("omemo bundle was received")
-        /////////////////
+        console.log("onbundle")
         let sk, sk_id, signature, ik, from_id, from , _key, pkey, pkey_id
         let pkeys = [] 
 
         $(stanza).find('items').each(function () {
             from_id = parseInt($(this).attr('node').split(":")[1])
-            console.log(from_id)
         })
         $(stanza).find('iq').each(function () {
             from = $(this).attr('from')
-            console.log(from)
         })
         $(stanza).find('signedPreKeyPublic').each(function () {
             sk_id = parseInt($(this).attr('signedPreKeyId'))
             sk = codec.Base64ToBuffer($(this).text())
-            console.log("signedKey")
-            console.log(sk)
-            console.log(sk_id)
         })
         $(stanza).find('signedPreKeySignature').each(function () {
             signature = codec.Base64ToBuffer($(this).text())
-            console.log("signature")
-            console.log(signature)
         })
-        console.log("trying to find prekeys")
         $(stanza).find('preKeyPub').each(function () {
             console.log("found pkey")
             pkey = codec.Base64ToBuffer($(this).text())
             pkey_id  = $(this).attr('keyId')
             _key = {id: pkey_id, key: pkey}
-            console.log(key_)
-          //  pkeys.put(key)
-          //  console.log(pkeys)
+            pkeys.put(_key)
         })
 
-       console.log("trying to find id key")
-       ik =  $(stanza).find('identityKey')           // codec.Base64ToBuffer($(this).text())
-       console.log("identityKey")
-       console.log(ik)
+        console.log(pkeys)
+        ik =  $(stanza).find('identityKey')           // codec.Base64ToBuffer($(this).text())
+        console.log(ik)
         ////////////////
         let public_bundle =  {
             registrationId: from_id,
@@ -161,60 +152,85 @@ Strophe.addConnectionPlugin('omemo', {
         }
 
         let res = { jid: from, public_bundle:  public_bundle }
+        console.log(res)
         return true 
     },
-    on_message: function (xml_stanza) {
+    on_device: function (stanza) {
+        if (omemo._id == undefined || stanza == null) { throw "on_device: stanza null or id not set " }
+        let id, appendand, from, were_in = false
+        let me = omemo._jid
+        let my_id = omemo._id
+        let ids = {}
 
-        console.log("omemo message update was received")
+        from = $(stanza).attr("from")
+        if (from == me) {
+        ids[my_id] = ''
+            $(stanza).find("device").each(function() {
+                var nid = $(this).attr('id')
+                ids[nid] = ''
+                if (nid == my_id) {
+                    were_in = true
+                }
+            })
+            if (were_in) { 
+                return true 
+            }
+            var res = $iq({type: 'set', from: omemo._jid, id: 'anounce1'})
+                .c('pubsub',    {xmlns: 'http://jabber.org/protocol/pubsub'})
+                .c('publish',   {node: Strophe.NS.OMEMO_DEVICELIST})
+                .c('item')
+                .c('list',      {xmlns: Strophe.NS.OMEMO})
+
+            for (var i in ids) {
+                res.c('device', {id: i }).up()
+            }
+            omemo.connection.send(res)
+            return true
+        }
+        $(stanza).find("device").each(function() {
+             var tid = $(this).attr('id')
+             ids[tid] = ''
+        })
+        localStorage.setItem(from, JSON.stringify(ids))
         return true
     },
-    on_device: function (xml_stanza) {
-
-        console.log("omemo device list update was received")
+    on_message: function (stanza) {
+        console.log("onemessage")
         return true
     },
+
     on_headline: function (stanza) {
-        let moo =   $(parsed).find('list')
-
-        console.log(moo)
-
-        //      .each(function () {
-
-        //  })
-
+        if(omemo.is_device(stanza)) {
+            omemo.on_device(stanza)
+            return true
+        }
+        else if(omemo.is_bundle(stanza)) {
+            omemo.on_bundle(stanza)
+            return true
+        }
         return true 
     },
-    is_deviceHeadline: function (stanza)  {
-
-        let parsed = $.parseXML(XML)
-
-        return false
-    },
-    is_bundleHeadline: function (stanza) {
-
-        return false
-    },
-    fetch_bundle: function (to, device) {
-        let ctxt = this
-        let res = $iq({type: 'get', from: ctxt._jid, to: to, id: 'fetch1'})
-            .c('pubsub', {xmlns: 'http://jabber.org/protocol/pubsub'})
-            .c('items', {node: Strophe.NS.OMEMO_BUNDLES + ":" + device}) 
-        this.connection.send(res)
+    fetch_bundle: function (to) {
+        let res = ''
+        let ids = JSON.parse(localStorage.getItem(to))
+        for (let i in ids) {
+            res = $iq({type: 'get', from: omemo._jid, to: to, id: 'fetch1'})
+                .c('pubsub', {xmlns: 'http://jabber.org/protocol/pubsub'})
+                .c('items', {node: Strophe.NS.OMEMO_BUNDLES + ":" + i}) 
+            omemo.connection.send(res)
+        }
     },
     refresh_bundle: function () {
 
-        console.log("omemo refreshing bundle")
     },
     publish_bundle: function () {
-        console.log("omemo announcing bundle")
-        let ctxt= this
         let sk_id = 1
 
         let promises = [
-            this.connection._signal_store.loadSignedPreKey(sk_id),
-            this.connection._signal_store.getIdentityKeyPair(),
-            this.connection._signal_store.loadSignedPreKeySignature(sk_id),
-            this.connection._signal_store.getLocalRegistrationId()
+            omemo.connection._signal_store.loadSignedPreKey(sk_id),
+            omemo.connection._signal_store.getIdentityKeyPair(),
+            omemo.connection._signal_store.loadSignedPreKeySignature(sk_id),
+            omemo.connection._signal_store.getLocalRegistrationId()
         ]
         return Promise.all(promises).then(o => {
             let sk = o[0]
@@ -224,9 +240,9 @@ Strophe.addConnectionPlugin('omemo', {
             let signature64 = codec.BufferToBase64(signature)
             let sk64 = codec.BufferToBase64(sk.pubKey)
             let ik64 = codec.BufferToBase64(ikp.pubKey)
-            let res = $iq({type: 'set', from: ctxt._jid, id: 'anounce2'})
+            let res = $iq({type: 'set', from: omemo._jid, id: 'anounce2'})
                 .c('pubsub', {xmlns: 'http://jabber.org/protocol/pubsub'})
-                .c('publish', {node: Strophe.NS.OMEMO_BUNDLES + ":" + this._id})
+                .c('publish', {node: Strophe.NS.OMEMO_BUNDLES + ":" + omemo._id})
                 .c('item')
                 .c('bundle', {xmlns: Strophe.NS.OMEMO })
                 .c('signedPreKeyPublic', {signedPreKeyId: sk_id}).t(sk64).up()
@@ -235,49 +251,56 @@ Strophe.addConnectionPlugin('omemo', {
                 .c('prekeys')
 
 
-            let keys = this.connection._signal_store.getPreKeyBundle(ctxt)
+            let keys = omemo.connection._signal_store.getPreKeyBundle(omemo)
             keys.forEach(function(key) {
                 res = res.c('preKeyPub', {'keyId': key.keyId}).t(codec.BufferToBase64(key.pubKey)).up()
             })
 
-            this.connection.send(res)
+            omemo.connection.send(res)
         })
 
     },
     publish_device: function () {
-        console.log("publishing device")
         var list = $build('list', {xmlns: Strophe.NS.OMEMO})
-        list.c('device', {id: this._id})
-        this.publish(Strophe.NS.OMEMO_DEVICELIST, [{
+        list.c('device', {id: omemo._id})
+        omemo.connection.pep.publish(Strophe.NS.OMEMO_DEVICELIST, [{
             data: list.tree()
         }], null)
     },
-    genPreKeys: function(range) {
-        console.log("generating prekeys")
+    gen_prekeys: function(range) {
         let kh = libsignal.KeyHelper
         let promises = []
-        for (let i = 3; i < range + 1; i++ ) {
+        for (let i = 1; i < range + 1; i++ ) {
             promises.push(kh.generatePreKey(i).then((k) =>  {
                 let key = {
                     pubKey: k.keyPair.pubKey, 
                     privKey: k.keyPair.privKey, 
                     keyId: k.keyId
                 }
-                this.connection._signal_store.storePreKey(i,key)
+                omemo.connection._signal_store.storePreKey(i,key)
             }))
         }
-        return Promise.all(promises).then(console.log("preKeys phase complete"))
+        return Promise.all(promises)
     },
-    new_id: function () {
-        console.log("generating device id")
+    gen_id: function () {
         let minDeviceId = 1
         let maxDeviceId = 2147483647
         let diff = (maxDeviceId - minDeviceId)
         let res = Math.floor(Math.random() * diff  + minDeviceId)
-        this._id = res
+        omemo._id = res
         return res
     },
+    is_device: function (stanza)  {
+        
+        return $(stanza).find("list").length != 0
+    },
+    is_bundle: function (stanza)  {
+        return $(stanza).find("bundle").length != 0
+    },
 
-});
+
+}
+
+Strophe.addConnectionPlugin('omemo', omemo)
 
 
